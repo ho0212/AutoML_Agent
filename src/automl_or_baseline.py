1f84b485ca119c391623cda2ec30d3c8c947bacc
MaloneHo <yhoo0040@student.monash.edu>
2025-08-28 21:30:47 +1000

    update automl_or_baseline.py

--- src/automl_or_baseline.py
+++ src/automl_or_baseline.py
@@ -12,3 +12,34 @@ def run_automl_or_baseline(problem: str, preprocessor, X_train, y_train, X_test,
     This function tries auto-sklearn if available; else run simple baselines.
     Returns: best_model, metrics(dict), model_name(str)
     """
+
+    # Try Auto-sklearn 
+    try:
+        if problem == "Classification":
+            model = Pipeline([
+                ("pre", preprocessor),
+                ("clf", AutoSklearnClassifier(time_left_for_this_task=180, per_run_time_limit=30))
+            ])
+            model.fit(X_train, y_train)
+            y_pred = model.predict(X_test)
+
+            metrics = {
+                "accuracy": float(accuracy_score(y_test, y_pred)),
+                "f1_macro": float(f1_score(y_test, y_pred, average="macro"))
+            }
+            return model, metrics, "AutoSklearnClassifier"
+        else:
+            model = Pipeline([
+                ("pre", preprocessor),
+                ("reg", AutoSklearnRegressor(time_left_for_this_task=180,per_run_time_limit=30))
+            ])
+            model.fit(X_train, y_train)
+            y_pred = model.predict(X_test)
+            rmse = mean_squared_error(y_test, y_pred, squared=False)
+            metrics = {"rmse": float(rmse)}
+            return model, metrics, "AutoSklearnRegressor"
+    except Exception as e:
+        warnings.warn(f"Auto-sklearn unavailable or failed, using baselines. Reason: {e}")
+    
+    # Baselines
+    
